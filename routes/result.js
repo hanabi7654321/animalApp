a12b565701fed9641a099827051c04c3f3ea6d41
Hanabi <hanabi1a21a2@gmail.com>
2021-01-31 04:22:03 +0900

    first commit

---
+++ routes/result.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET result page. */
+router.get('/', function(req, res, next) {
+  const animals = ["あざらし", "ねこ", "いぬ", "パンダ", "ペンギン"];
+  const num = Math.floor(Math.random() * animals.length) + 1;
+  const animal = animals[num];
+  const animalImg = "public/images/pic1" ;
+  res.render('result', { 
+    root: animal,
+    animalImg: animalImg
+  });
+});
+
+module.exports = router;
