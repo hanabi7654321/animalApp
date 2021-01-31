941528193cf9a787385be23a76950a8ccdf4c1e8
Hanabi <hanabi1a21a2@gmail.com>
2021-01-31 22:57:57 +0900

    make it better

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 
-const port = process.env.PORT || 8000;
-
 router.get('/', function(req, res, next) { 
   res.render('index', { title: '動物おみくじ' });
 });
